e155f04237fa8d02f2c457d35245b30ece931e44
Thomas Schaffner <tfs3@cs.princeton.edu>
2017-12-17 01:23:06 -0500

    Code to compute very basic linear regression given very restrictive/specific file formatting constraints

--- fairness/fairvis/static/fairvis/script.js
+++ fairness/fairvis/static/fairvis/script.js
@@ -1,16 +1,123 @@
-// $("#upload-form").submit(function (event) {
-//     event.preventDefault();
-//     var frm = $("#upload-form");
-//     $.ajax({
-//         type: "POST",
-//         url: frm.attr("action"),
-//         data: frm.serialize(),
-//         // Probably not the right way to specify callbacks here
-//         success: function (data) {
-//             console.log(data);
-//         },
-//         error: function (error) {
-//             console.log(error);
+// Ref: https://www.codexworld.com/ajax-file-upload-with-form-data-jquery-php-mysql/
+// $(document).ready(function(e){
+//     $("#fupForm").on('submit', function(e){
+//         e.preventDefault();
+//         $.ajax({
+//             type: 'POST',
+//             url: 'submit.php',
+//             data: new FormData(this),
+//             contentType: false,
+//             cache: false,
+//             processData:false,
+//             beforeSend: function(){
+//                 $('.submitBtn').attr("disabled","disabled");
+//                 $('#fupForm').css("opacity",".5");
+//             },
+//             success: function(msg){
+//                 $('.statusMsg').html('');
+//                 if(msg == 'ok'){
+//                     $('#fupForm')[0].reset();
+//                     $('.statusMsg').html('<span style="font-size:18px;color:#34A853">Form data submitted successfully.</span>');
+//                 }else{
+//                     $('.statusMsg').html('<span style="font-size:18px;color:#EA4335">Some problem occurred, please try again.</span>');
+//                 }
+//                 $('#fupForm').css("opacity","");
+//                 $(".submitBtn").removeAttr("disabled");
+//             }
+//         });
+//     });
+    
+//     //file type validation
+//     $("#file").change(function() {
+//         var file = this.files[0];
+//         var imagefile = file.type;
+//         var match= ["image/jpeg","image/png","image/jpg"];
+//         if(!((imagefile==match[0]) || (imagefile==match[1]) || (imagefile==match[2]))){
+//             alert('Please select a valid image file (JPEG/JPG/PNG).');
+//             $("#file").val('');
+//             return false;
 //         }
-//     })
-// })
+//     });
+// });
+
+var data;
+var namefileSelected = false;
+var datafileSelected = false;
+var predictfileSelected = false;
+
+$(document).ready(function() {
+	data = {};
+
+	$("#upload-form").on('submit', function(e) {
+		e.preventDefault();
+
+		if (namefileSelected && datafileSelected) {
+			$.ajax({
+				type: 'POST',
+				url: $(this).attr('action'),
+				data: new FormData(this),
+				contentType: false,
+				cache: false,
+				processData: false,
+				beforeSend: function() {
+					console.log("sending");
+				},
+				success: function(msg) {
+					console.log(msg);
+				}
+			})
+		} else {
+			alert("Please upload at least a name file and data file");
+		}
+	});
+
+	$("#namefile").change(function() {
+		if ($("#namefile").val()) {
+			namefileSelected = true;
+		} else {
+			namefileSelected = false;
+		}
+
+		if (namefileSelected && datafileSelected) {
+			enableFormSubmit();
+		}
+	});
+
+	$("#datafile").change(function() {
+		if ($("#datafile").val()) {
+			datafileSelected = true;
+		} else {
+			datafileSelected = false;
+		}
+
+		if (namefileSelected && datafileSelected) {
+			enableFormSubmit();
+		}
+	});
+
+	$("#predictfile").change(function() {
+		if ($("#predictfile").val()) {
+			predictfileSelected = true;
+		} else {
+			predictfileSelected = false;
+		}
+	});
+})
+
+
+function enableFormSubmit() {
+	$("#dataSubmit").attr('disabled', false);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- fairness/fairvis/views.py
+++ fairness/fairvis/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
+from sklearn.linear_model import LinearRegression
+
 # Create your views here.
 
 # def index(request):
@@ -24,14 +26,68 @@ def index(request):
 
 def upload(request):
     if request.method == 'POST':
+        nfile = request.FILES['namefile']
+        names = parse_file(nfile)
         dfile = request.FILES['datafile']
+        data = parse_file(dfile)
+        # TODO(tfs): Not yet handling their internal prediction file (conditionally)
         response_data = {}
-        response_data['contents'] = []
-        for line in dfile.open():
-            response_data['contents'].append(line.decode('utf-8').strip().split(','))
+        response_data['nameFile'] = names
+        response_data['dataFile'] = data
+        response_data['linregPredictions'] = linear_prediction(parse_file(dfile)).tolist()
         return JsonResponse(response_data)
     else:
         return HttpResponse("Nope")
+
+
+
+
+
+def parse_file(f):
+    if f is None:
+        return None
+
+    data = []
+    for line in f:
+        newline = line.decode('utf-8').strip().split(',')
+
+        for i in range(len(newline)):
+            if not newline[i]:
+                newline[i] = "NA"
+
+        if len(newline) == 1:
+            newline = newline[0]
+        data.append(newline)
+
+    return data
+
+
+
+
+def linear_prediction(data):
+    # TODO(tfs): How do we split up test/train? How do we handle their predictions, similarly?
+    train_data     = []
+    train_outcomes = []
+    predict_data   = []
+
+
+    for line in data:
+        if line[-1] is not "NA":
+            print(line)
+            train_data.append([float(e) for e in line[:-1]])
+            train_outcomes.append(float(line[-1]))
+        predict_data.append([float(e) for e in line[:-1]])
+
+
+    print(train_data)
+    print(train_outcomes)
+    print(predict_data)
+
+    linreg = LinearRegression()
+    fit = linreg.fit(train_data, train_outcomes)
+    predictions = linreg.predict(predict_data)
+
+    return predictions
 
     
 
