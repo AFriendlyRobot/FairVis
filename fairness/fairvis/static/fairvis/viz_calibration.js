c38a9d5b664d13397ba8e49d5331a95f4093645b
Thomas Schaffner <t.f.schaffner@gmail.com>
2018-01-09 16:02:01 -0500

    Working on calibration

--- fairness/fairvis/static/fairvis/viz_calibration.js
+++ fairness/fairvis/static/fairvis/viz_calibration.js
@@ -2,6 +2,10 @@ var cal_settings = {};
 var cal_data = {};
 
 
+var CAL_PLOT_WIDTH = 800;
+var CAL_PLOT_HEIGHT = 600;
+
+
 function calibration_initialize() {
 	// Not using let, don't want to worry about potential adoption rate issues
 	var cname;
@@ -30,41 +34,69 @@ function calibration_initialize() {
 	}
 
 	// Populate class value selection options
-	$("#calibration-protected-selection").change(updateCalibrationClassSelection);
+	// $("#calibration-protected-selection").change(updateCalibrationClassSelection);
 
 	// Set initial value for protected field
 	$("#calibration-protected-selection").val(json.colNames[0]);
 	$("#calibration-prediction-selection").val(modelNames[0]);
 
-	updateCalibrationClassSelection();
+	// updateCalibrationClassSelection();
 
 }
 
 
-function updateCalibrationClassSelection() {
-	$("#calibration-class-value-selection").empty();
+// Ref: https://bl.ocks.org/mbostock/3885304
+// Ref: https://bl.ocks.org/mbostock/3887051
+function draw_calibration() {
+	var cal_svg = d3.select("#calibration-svg"),
+		cmargin = { top: 20, right: 20, bottom: 30, left: 40 },
+		cwidth = +svg.attr("width") - margin.left - margin.right,
+		cheight = +svg.attr("height") - margin.top - margin.bottom,
+		g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var colName = $("#calibration-protected-selection").val();
-	var point;
+	svg.attr("width", CAL_PLOT_WIDTH);
+	svg.attr("height", CAL_PLOT_HEIGHT);
 
-	options = [];
+	var cx0 = d3.scaleBand()
+	            .rangeRound([0, cwidth])
+	            .paddingInner(0.1);
 
-	for (var i = 0; i < json.dataPoints.length; i++) {
-		point = json.dataPoints[i];
-		if (!options.includes(point["data"][colName])) {
-			options.push(point["data"][colName]);
-		}
-	}
+	var cx1 = d3.scaleBand()
+	            .padding(0.05);
 
-	for (var i = 0; i < options.length; i++) {
-		optName = options[i];
-		newOpt  = "<option class=\"calibration-class-value-option\" value=\"";
-		newOpt += optName + "\">" + optName + "</option>";
-		$("#calibration-class-value-selection").append(newOpt);
-	}
+	var cy = d3.scaleLinear()
+	           .rangeRound([cheight, 0]);
 
-	$("#calibration-class-value-selection").val(options[0]);
+	// TODO(tfs): Color scale for bars based on number of options
+	// var cz = d3.scaleOridinal()
+	//            .range
 }
+
+
+// function updateCalibrationClassSelection() {
+// 	$("#calibration-class-value-selection").empty();
+
+// 	var colName = $("#calibration-protected-selection").val();
+// 	var point;
+
+// 	options = [];
+
+// 	for (var i = 0; i < json.dataPoints.length; i++) {
+// 		point = json.dataPoints[i];
+// 		if (!options.includes(point["data"][colName])) {
+// 			options.push(point["data"][colName]);
+// 		}
+// 	}
+
+// 	for (var i = 0; i < options.length; i++) {
+// 		optName = options[i];
+// 		newOpt  = "<option class=\"calibration-class-value-option\" value=\"";
+// 		newOpt += optName + "\">" + optName + "</option>";
+// 		$("#calibration-class-value-selection").append(newOpt);
+// 	}
+
+// 	$("#calibration-class-value-selection").val(options[0]);
+// }
 
 
 
