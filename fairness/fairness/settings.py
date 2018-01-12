9b83d1ecd72382f3c8fd70b81cef6e2028ada1d1
Thomas Schaffner <t.f.schaffner@gmail.com>
2018-01-12 14:50:36 -0500

    Minor clean

--- fairness/fairness/settings.py
+++ fairness/fairness/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['fairviz.herokuapp.com']
+ALLOWED_HOSTS = ['fairviz.herokuapp.com', 'localhost']
 
 
 # Application definition
